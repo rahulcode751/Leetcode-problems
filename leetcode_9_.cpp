131dc1feafbbb212225ae8386391b1b0a940e58e
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2021-09-10 15:56:50 +0530

    Add files via upload

---
+++ leetcode_9_.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int r = 0;
+        long long int s = 0;
+        int a = x;
+        if(x < 0){
+            return false;
+        }
+
+        while(x > 0){
+            r = x%10;
+            s = s*10 + r;
+            x = x/10;
+        }
+        if(s == a){
+            return true;
+        }
+        return false;
+    }
+};
