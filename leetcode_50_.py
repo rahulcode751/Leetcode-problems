c4edfccf4ce1fd8c81200aefd2e01e224e3fcd70
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2021-09-11 08:15:27 +0530

    Update leetcode_50_.py

--- leetcode_50_.py
+++ leetcode_50_.py
@@ -1,4 +1,4 @@
-#python3
+#python3  leetcode problem 50 i have done it in c++ please go through that
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         return x**n    # it can also be done using loops but its a simple and best approach
