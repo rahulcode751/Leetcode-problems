95847cd7375ab8c44e5b4094973aea17aaec7da2
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2021-09-13 09:22:32 +0530

    Add files via upload

---
+++ binary_mod_2.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& a, int key) {
+        int s = 0;
+        int e = a.size() - 1;
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(a[mid] == key){
+                return mid;
+            }
+            
+            if(a[s]<=a[mid]){
+                if(key>=a[s] && key<=a[mid]){
+                    e = mid - 1;
+                }else{
+                    s = mid + 1;
+                }
+            } else {
+                if(key>=a[mid] && key<=a[e]){
+                    s = mid + 1;
+                }else{
+                    e = mid - 1; 
+                }
+            }
+            
+        }
+        return -1;
+    }
+};
+int main()
+{
+	Solution A;
+	vector<int> v(5,0);
+	v.at(0) = 3;
+	v.at(1) = 4;
+	v.at(2) = 5;
+	v.at(3) = 1;
+	v.at(4) = 2;
+	
+	cout<<A.search(v,1);
+}
