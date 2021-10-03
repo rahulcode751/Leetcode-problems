60c3038d196a6c758f14a4c434ae69ed21ae3771
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2021-10-03 09:00:30 +0530

    Add files via upload

---
+++ leetcode_867.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int row = matrix.size(), col = matrix[0].size();
+        //intializing a vector with 'col' rows and 'row' columns
+        vector<vector<int>> result(col, vector<int>(row, 0));
+        for(int i = 0; i < col; i++)
+            for(int j = 0; j < row; j++)
+                result[i][j] = matrix[j][i];
+        return result;
+    }
+};
