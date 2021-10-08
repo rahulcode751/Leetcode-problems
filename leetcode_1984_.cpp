ff8dd33855b5d12b099f19f0da9fc36cf44f1c87
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2021-10-08 21:17:53 +0530

    Add files via upload

---
+++ leetcode_1984_.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        --k;
+        int mindiff=100000;
+        for(int i=0; i<nums.size()-k; ++i)
+            mindiff=min(mindiff,nums[i+k]-nums[i]);
+        return mindiff;
+    }
+};
